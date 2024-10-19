2bdb53e31e4abad95c58bec6a0fdd035df0bdb2b
ttv22 <ttv22@cam.ac.uk>
2024-10-19 14:19:50 +0100

    Use custom 2D U-Net

--- scripts/mri/model_loader.py
+++ scripts/mri/model_loader.py
@@ -1,10 +1,12 @@
 import torch
 from typing import Union, Optional, Literal
+from pathlib import Path
 
 from utils.makepath import makepath as mkp
 from config.config_loader import load_config
 from networks.mri_pdhg_net import MriPdhgNet
-from networks.unet import UNet
+# from networks.unet import UNet
+from networks.unet_2d import UNet2d
 
 
 class ModelLoader:
@@ -51,16 +53,23 @@ class ModelLoader:
         full_model_path = mkp(model_folder, model_filename)
         return full_model_path
 
-    def load_pretrained_model(self) -> MriPdhgNet:
+    def load_pretrained_model(
+            self, root_dir: Optional[Union[str, Path]]) -> MriPdhgNet:
         is_state_dict = self.config["log"]["is_state_dict"]
-        full_path = self.get_full_model_path()
+        full_path = mkp(root_dir, self.get_full_model_path())
         if is_state_dict:
             model = self.init_new_model()
-            state_dict = torch.load(full_path)
+            state_dict = torch.load(
+                full_path,
+                map_location=self.device()
+            )
             model.load_state_dict(state_dict)
         else:
-            model = torch.load(full_path)
-        return model.to(self.device())
+            model = torch.load(
+                full_path,
+                map_location=self.device()
+            )
+        return model
 
     def init_new_model(self) -> MriPdhgNet:
         pdhg_config = self.config["pdhg"]
@@ -88,20 +97,34 @@ class ModelLoader:
         )
         return tgv_pdhg_net.to(self.device())
 
-    def init_unet(self) -> UNet:
+    # def init_unet(self) -> UNet:
+    def init_unet(self) -> UNet2d:
         unet_config = self.config["unet"]
-        unet = UNet(
-            dim=2,
-            n_ch_in=unet_config["in_channels"],
-            n_ch_out=unet_config["out_channels"],
-            n_filters=unet_config["init_filters"],
-            n_enc_stages=unet_config["n_blocks"],
-            n_convs_per_stage=2,
-            kernel_size=3,
-            res_connection=False,
-            bias=True,
-            padding_mode="zeros"
+        # unet = UNet(
+        #     dim=2,
+        #     n_ch_in=unet_config["in_channels"],
+        #     n_ch_out=unet_config["out_channels"],
+        #     n_filters=unet_config["init_filters"],
+        #     n_enc_stages=unet_config["n_blocks"],
+        #     n_convs_per_stage=2,
+        #     kernel_size=3,
+        #     res_connection=False,
+        #     bias=True,
+        #     padding_mode="zeros"
+        # ).to(self.device())
+
+        unet = UNet2d(
+            in_channels=unet_config["in_channels"],
+            out_channels=unet_config["out_channels"],
+            init_filters=unet_config["init_filters"],
+            n_blocks=unet_config["n_blocks"],
+            activation=unet_config["activation"],
+            downsampling_kernel=unet_config["downsampling_kernel"],
+            downsampling_mode=unet_config["downsampling_mode"],
+            upsampling_kernel=unet_config["upsampling_kernel"],
+            upsampling_mode=unet_config["upsampling_mode"],
         ).to(self.device())
+
         with torch.no_grad():
             # Make sure the bias works with complex numbers?
             def bias_to_zero(m):
