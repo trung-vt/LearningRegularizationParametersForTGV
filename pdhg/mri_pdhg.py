bdba419eb76541839db95a5c4cf4edd73f514195
trung-vt <trung-vt>
2024-10-26 23:26:42 +0000

    Added test class for MRI PDHG

--- pdhg/mri_pdhg.py
+++ pdhg/mri_pdhg.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
-from typing import Union, Optional, Dict, Any, Literal, Callable
+from typing import Union, Optional, Dict, Any, Literal, Callable, Tuple
 
 from encoding_objects.cart_2d_enc_obj import Cart2DEncObj
-from pdhg.pdhg import TvPdhgTorch, TgvPdhgTorch
+from pdhg.pdhg import TvPdhgTorch, TgvPdhgTorch, TvLambdaReg, TgvLambdaReg
 
 
 class MriPdhgTorch(nn.Module):
@@ -26,7 +26,10 @@ class MriPdhgTorch(nn.Module):
 
     def forward(
             self, num_iters: int,  # number of iterations
-            lambda_reg: Union[float, torch.Tensor],
+            lambda_reg: Union[
+                float, torch.Tensor, Dict[
+                    Literal["lambda1_v", "lambda0_w"],
+                    Union[float, torch.Tensor]]],
             kdata: torch.Tensor,  # measured data
             kmask: torch.Tensor,
             state: Union[torch.Tensor, Dict[str, Any]],
@@ -50,3 +53,39 @@ class MriPdhgTorch(nn.Module):
             return_dict=return_dict, tqdm_progress_bar=tqdm_progress_bar,
             device=device)
         return result
+
+
+class MriPdhgTorchTest(MriPdhgTorch):
+    def __init__(
+            self,
+            device: Union[str, torch.device],
+            pdhg_algorithm: Literal["tv", "tgv"],
+            num_iters: int,  # number of iterations
+            kdata: torch.Tensor,  # measured data
+            kmask: torch.Tensor,
+            state: Union[torch.Tensor, Dict[str, Any]],
+            csmap: Optional[torch.Tensor] = None,   # coil sensitivity map
+            sigma: Union[float, torch.Tensor] = 1.0 / 3,
+            tau: Union[float, torch.Tensor] = 1.0 / 3,
+            theta: Union[float, torch.Tensor] = 1.0,
+            tqdm_progress_bar: Optional[Callable] = None
+    ):
+        super().__init__(device=device, pdhg_algorithm=pdhg_algorithm)
+        self.num_iters = num_iters
+        self.kdata = kdata
+        self.kmask = kmask
+        self.state = state
+        self.csmap = csmap
+        self.sigma = sigma
+        self.tau = tau
+        self.theta = theta
+        self.tqdm_progress_bar = tqdm_progress_bar
+
+    def get_denoised(
+            self, lambda_reg: Union[TvLambdaReg, TgvLambdaReg]
+    ) -> torch.Tensor:
+        return super().forward(
+            num_iters=self.num_iters, lambda_reg=lambda_reg, kdata=self.kdata,
+            kmask=self.kmask, state=self.state, csmap=self.csmap,
+            sigma=self.sigma, tau=self.tau, theta=self.theta,
+            return_dict=False, tqdm_progress_bar=self.tqdm_progress_bar)
